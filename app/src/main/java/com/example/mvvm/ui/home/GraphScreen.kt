304b75be2c8374c4c9e2822b9d5b6870f644dab3
Minecraftian14 <mcxiv14@gmail.com>
2023-04-28 11:17:24 +0530

    Fixed Plotting Service

--- app/src/main/java/com/example/mvvm/ui/home/GraphScreen.kt
+++ app/src/main/java/com/example/mvvm/ui/home/GraphScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mvvm.ui.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -9,10 +8,9 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.mvvm.domain.item.EMItem
+import `in`.mcxiv.plotcompose.LineChart
 
 @Composable
 fun DisplayLineChart(navHostController: NavHostController, data : List<Float>){
@@ -24,9 +22,10 @@ fun DisplayLineChart(navHostController: NavHostController, data : List<Float>){
         verticalArrangement = Arrangement.Center
     ) {
         LineChart(
-            values = data,
+            values = data.takeLast(10),
             Brush.linearGradient(0f to Color(0xFF9336D1), 1f to Color(0x334a3097)),
             Color(0xFF9336D1),
+            showMarkers = false,
             modifier = Modifier
                 .fillMaxWidth()
                 .size(250.dp, 200.dp)
